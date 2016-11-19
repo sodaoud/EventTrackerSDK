f0b17fe8dbef36797b30ce54669dcb4c4f04dd08
Sofiane Daoud <daoud.sofiane@gmail.com>
2016-11-19 15:32:22 +0100

    first commit

---
+++ Sample/src/main/java/io/sodaoud/eventtracker/MainActivity.java
@@ -0,0 +1,13 @@
+package io.sodaoud.eventtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
